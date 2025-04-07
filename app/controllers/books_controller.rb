c393a10521648382a66dfdbeeeca97f3c5cd916c
monyu-01 <opsu.0102@gmail.com>
2025-04-07 09:18:56 +0000

    [add] 新規投稿機能の作成”

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,33 @@
 class BooksController < ApplicationController
   def new
+    @book = Book.new
   end
 
   def index
+    @books = Book.all
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def create 
+    @book = Book.new(book_params)
+
+    if @book.save
+      redirect_to books_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
